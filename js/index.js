0913cc4ee16ac628f3dd4441e9a841cf461ecf2c
cangowu <1138806090@qq.com>
2016-02-27 11:39:48 +0800

    shenme

--- js/index.js
+++ js/index.js
@@ -1,10 +1,6 @@
 (function () {
 
-
-    //第一次加载打开第一个界面
-    var first = document.querySelector('#footer div.btn');
-    cj.addClass(first, 'active');
-
+    firstReload();
     footerEventBind();
 
     function footerEventBind() {
@@ -13,12 +9,10 @@
         var btns = footer.getElementsByClassName('btn');
 
         for (var i = 0; i < btns.length; i++) {
-
             var btn = btns[i];
             (function (elm, idx) {
                 elm.addEventListener('touchstart', function (e) {
                     var className = this.getAttribute('class');
-
                     if (!cj.hasClass(this, 'active')) {
                         cj.addClass(this, 'active');
                         var siblings = this.parentNode.children;
@@ -28,13 +22,45 @@
                                 cj.removeClass(sb, 'active');
                             }
                         }
-//                        window.location.href = "goback.html";
+
+//                        var header = document.getElementById('header');
+//                        header.innerText = this.querySelector('span').innerText;
+
+                        var wrapper = document.getElementById('wrapper');
+                        var html = this.getAttribute('data-html');
+                        var wrapperHtml = readHTML('html/' + html);
+
+                        wrapper.innerHTML = wrapperHtml;
+//                        setTimeout(function(){
+//                            window.location.href = "goback.html";
+//                        },2000);
                     }
 
 
                 }, false);
             })(btn, i);
         }
+    }
 
+    function readHTML(url) {
+        var returnHtml = '';
+        var xmlhttp = new XMLHttpRequest();
+        xmlhttp.open("GET", url, false);
+        xmlhttp.onreadystatechange = function () {
+            if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+                returnHtml = xmlhttp.responseText;
+            }
+        }
+        xmlhttp.send();
+
+        return returnHtml;
+    }
+
+    function firstReload() {
+        //第一次加载打开第一个界面
+        var first = document.querySelector('#footer div.btn');
+        cj.addClass(first, 'active');
+        var wrapper = document.getElementById('wrapper');
+        wrapper.innerHTML = readHTML('html/statistics.html');
     }
 })();
